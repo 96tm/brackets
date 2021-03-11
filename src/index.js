2fcfcf14b6e3b96bd4016ba8f8b318853ceeda45
96tm <khabcity@gmail.com>
2021-03-11 19:58:31 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const conf = new Map(bracketsConfig);
+    const openers = new Set(conf.keys());
+    let stack = [];
+    for (brace of str) {
+        if (!stack.length) {
+            stack.push(brace);
+        } else {
+            let top = stack[stack.length - 1];
+            if (conf.get(top) === brace) {
+                stack.pop();
+            } else if (!openers.has(brace)) return false;
+            else {
+                stack.push(brace);
+            }
+        }
+    }
+    return !stack.length;
+};
